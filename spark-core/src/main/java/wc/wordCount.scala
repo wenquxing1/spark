92a840f1eb9a84d1e071720b1bb6ea229bdc2b0c
wenquxing1 <2535410573@qq.com>
2023-09-13 19:33:57 +0800

    ep01

---
+++ spark-core/src/main/java/wc/wordCount.scala
@@ -0,0 +1,26 @@
+package wc
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object wordCount {
+  def main(args: Array[String]): Unit = {
+    System.setProperty("hadoop.home.dir", "null")
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("WordCount")
+    val sc = new SparkContext(sparkConf)
+
+    val lines: RDD[String] = sc.textFile("datas")
+    val words: RDD[String] = lines.flatMap(_.split(" "))
+
+    val wordGroup: RDD[(String, Iterable[String])] = words.groupBy(word => word)
+
+    val wordToCount = wordGroup.map{
+      case (word, list) =>{(word, list.size)}
+    }
+
+    val array: Array[(String, Int)] = wordToCount.collect()
+    array.foreach{ case (word, count) => println(s"$word: $count") }
+
+    sc.stop();
+  }
+}
