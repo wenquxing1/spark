e5d95477a42a28de87e94c7a2692eecc15fa72c0
wenquxing1 <2535410573@qq.com>
2023-10-15 19:29:01 +0800

    ep02

---
+++ spark-core/src/main/java/com/atguigu/spark/core/test/Executor.scala
@@ -0,0 +1,23 @@
+package com.atguigu.spark.core.test
+
+import java.io.{InputStream, ObjectInputStream}
+import java.net.{ServerSocket, Socket}
+
+object Executor {
+  def main(args: Array[String]): Unit = {
+    //启动服务器，接收数据
+    val server = new ServerSocket(9999)
+    println("服务器启动等待接收")
+    //等待客户端的连接
+    val client: Socket = server.accept()
+    val in: InputStream = client.getInputStream
+    val objIn = new ObjectInputStream(in)
+    val task: Task = objIn.readObject().asInstanceOf[Task]
+    val ints: List[Int] = task.compute()
+    println("计算节点的计算结果为：" + ints)
+    objIn.close()
+    client.close()
+    server.close()
+
+  }
+}
---
+++ spark-core/src/main/java/com/atguigu/spark/core/test/Task.scala
@@ -0,0 +1,13 @@
+package com.atguigu.spark.core.test
+
+class Task extends  Serializable {
+  val datas = List(1,2,3,4)
+
+  //val logic = (num: Int) => {num * 2}
+  val logic : (Int)=>Int = _ * 2
+
+  //计算
+  def compute() = {
+    datas.map(logic)
+  }
+}
